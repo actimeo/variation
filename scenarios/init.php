93e96385b32b876d67ecad185db594396fd89df7
Philippe MARTIN <pmartin@kavarna.fr>
2014-07-01 21:39:30 +0200

    debug date affectation usager à groupe

--- scenarios/init.php
+++ scenarios/init.php
@@ -90,7 +90,7 @@ $per_id = $base->personne_ajoute ($token, 'usager');
 $base->personne_info_varchar_set ($token, $per_id, 'nom', 'DURAND', $uti_id_variation);
 $base->personne_info_varchar_set ($token, $per_id, 'prenom', 'Pierre', $uti_id_variation);
 
-$base->personne_groupe_ajoute ($token, $per_id, $grp_id, '01/01/2104', NULL, NULL, 
+$base->personne_groupe_ajoute ($token, $per_id, $grp_id, '01/01/2014', NULL, NULL, 
 			       2 /* accepté */, NULL, NULL, NULL, NULL);
 /* Ajout d'un employé */
 $per_id = $base->personne_ajoute ($token, 'personnel');
